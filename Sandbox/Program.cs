090a0f233095e9edffbb48eca7be80f5f80827b7
Eugene Bikkinin <xnegxneg@gmail.com>
2021-01-27 12:54:18 +0300

    Add project files.

---
+++ Sandbox/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Locks;
+
+namespace Sandbox
+{
+    internal static class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var buckets = new long[2];
+            ILock @lock = new Peterson();
+
+            var threads = Enumerable.Range(0, 2).Select(i =>
+            {
+                return new Thread(() =>
+                {
+                    while (true)
+                    {
+                        @lock.Lock();
+                        buckets[i]++;
+                        // Thread.Sleep(100);
+                        @lock.Unlock();
+                    }
+                });
+            });
+            
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+            
+            while (true)
+            {
+                Console.Clear();
+                foreach (var bucket in buckets)
+                {
+                    Console.WriteLine(bucket);
+                }
+                
+                await Task.Delay(500);
+            }
+        }
+    }
+}
