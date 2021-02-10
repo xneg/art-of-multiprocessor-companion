210e07e85427799e014245c217e48250a8b9422e
Eugene Bikkinin <xnegxneg@gmail.com>
2021-02-10 16:52:55 +0300

    Refactor DijkstraLock.

--- Locks/DijkstraLock.cs
+++ Locks/DijkstraLock.cs
@@ -4,39 +4,39 @@ namespace Locks
 {
     public class DijkstraLock: ILock
     {
-        private readonly bool[] _b;
-        private readonly bool[] _c;
+        private readonly bool[] _first;
+        private readonly bool[] _second;
         private int _k;
 
         public DijkstraLock(int count)
         {
-            _b = Enumerable.Range(0, count).Select(_ => true).ToArray();
-            _c = Enumerable.Range(0, count).Select(_ => true).ToArray();
+            _first = new bool[count];
+            _second = new bool[count];
         }
         public void Lock()
         {
             var me = ThreadId.Get();
-            _b[me] = false;
+            _first[me] = true;
 
-            do
+            while (!_second[me] || _second.Where((_, k) => k != me).Any(v => v))
             {
                 if (_k != me)
                 {
-                    _c[me] = true;
-                    if (_b[_k]) { _k = me; }
+                    _second[me] = false;
+                    if (!_first[_k]) { _k = me; }
                 }
                 else
                 {
-                    _c[me] = false;
+                    _second[me] = true;
                 }
-            } while (_k != me || _c[me] || _c.Where((_, k) => k != me).Any(v => !v));
+            } 
         }
 
         public void Unlock()
         {
             var me = ThreadId.Get();
-            _c[me] = true;
-            _b[me] = true;
+            _second[me] = false;
+            _first[me] = false;
         }
     }
 }
